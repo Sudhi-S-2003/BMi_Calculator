200c54c15ea85f3a336e71f9db7fcf35b88bad93
Sudhi S. <sudhiselampa19@gmail.com>
2024-06-06 09:07:57 +0530

    ''

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.getElementById('no-display').style.display='none';
+function cal(){
+    let weight=document.getElementById('weight').value;
+    let height=document.getElementById('height').value;
+    height_in_m=height/100;
+    let bmi=weight/(height_in_m*height_in_m);
+    if (bmi < 18.5) {
+        document.getElementById('result-head').innerHTML="Underweight";
+        document.getElementById('result').innerHTML="Your weight is underweight";
+    } else if (bmi < 24.9) {
+        document.getElementById('result-head').innerHTML="Normal weight";
+        document.getElementById('result').innerHTML="Your weight is normal weight";
+    } else if (bmi < 29.9) {
+        document.getElementById('result-head').innerHTML="Overweight";
+        document.getElementById('result').innerHTML="Your weight is overweight";
+    } else {
+        document.getElementById('result-head').innerHTML="Obese";
+        document.getElementById('result').innerHTML="You are obese.";
+    }
+  
+    document.getElementById('no-display').style.display='block';
+}
